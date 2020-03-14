1431e0060eb86825ac33b09a2618f9e95a071089
Iuri Pereira <iuri.3d@gmail.com>
2020-03-14 20:42:24 +0000

    media queries

--- js/script.js
+++ js/script.js
@@ -1,17 +1,26 @@
 $(document).ready(function(){
 
+    //////////////Display Login Modal/////////
     $('.login').on('click', function(){
         $('#myModal').css('display', 'flex');
     });
 
+    //////////////Close Login Modal//////////
     $('.close').on('click', function(){
         $('#myModal').css('display', 'none');
     });
 
+    /////////////Expand tickets////////////////////
     $('.ticket-open').on('click',function(){
         $(this).parent().siblings('.collapsible').toggleClass('down');
     });
    
+    ////// Toggle side bar open //////
+    $(".hamburger").click(function() {
+        $(".sidebar").toggleClass("open");
+        $(".contentbox").toggleClass("open");
+    
+    });
 
 
 })
