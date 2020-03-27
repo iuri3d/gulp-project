e91c87181bf76aba75a3abb5a66d97b8556a5d11
Iuri <iuri.3d@gmail.com>
2020-03-27 12:12:12 +0000

    adicionar productos styling, corrigir modals, ajustar routings

--- js/script.js
+++ js/script.js
@@ -7,8 +7,48 @@ $(document).ready(function(){
     });
 
     //////////////Close Login Modal//////////
-    $('.close').on('click', function(){
+    $('#myModal .close').on('click', function(){
         $('#myModal').css('display', 'none');
+    });
+
+    //////////////Open Compose modal//////////////
+
+    $('#compose-button').on('click', function() {
+        $('#modalCompose').css('display', 'block');
+        $('body').addClass('modalOpen');
+    });
+
+    ///////////////Close Compose Modal ////////////
+
+    $('#modalCompose .close').on('click', function() {
+        $('#modalCompose').css('display', 'none');
+        $('body').removeClass('modalOpen');
+    });
+
+    ///////////////Open Event Modal ////////
+    $('#event-button').on('click', function(){
+        $('#modalEvent').css('display', 'block');
+        $('body').addClass('modalOpen');
+    });
+
+    /////////////////Close Event Modal ///////////
+    $('#modalEvent .close').on('click', function(){
+        $('#modalEvent').css('display', 'none');
+        $('body').removeClass('modalOpen');
+    });
+
+    ///////////////Open Ticket Modal ///////////////
+
+    $('#ticket-button').on('click', function(){
+        $('#modalTickets').css('display', 'block');
+        $('body').addClass('modalOpen');
+    });
+
+    //////////////Close Ticket Modal////////////////
+
+    $('#modalTickets .close').on('click', function(){
+        $('#modalTickets').css('display', 'none');
+        $('body').removeClass('modalOpen');
     });
 
     /////////////Expand tickets////////////////////
@@ -31,47 +71,49 @@ $(document).ready(function(){
     });
 
 
+    if ( document.getElementById("home-chart") ){
+
     const homeChart = document.getElementById("home-chart").getContext('2d');
 
-    let chart01 = new Chart ( homeChart, {
-        type: 'horizontalBar',
-    data: {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-        datasets: [{
-            label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
-            borderWidth: 1
-        }]
-    },
-    responsive: true,
-    maintainAspectRatio: false,
-    options: {
-        scales: {
-            yAxes: [{
-                ticks: {
-                    beginAtZero: true
-                }
+        let chart01 = new Chart ( homeChart, {
+            type: 'horizontalBar',
+        data: {
+            labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+            datasets: [{
+                label: '# of Votes',
+                data: [12, 19, 3, 5, 2, 3],
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(153, 102, 255, 0.2)',
+                    'rgba(255, 159, 64, 0.2)'
+                ],
+                borderColor: [
+                    'rgba(255, 99, 132, 1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(153, 102, 255, 1)',
+                    'rgba(255, 159, 64, 1)'
+                ],
+                borderWidth: 1
             }]
+        },
+        responsive: true,
+        maintainAspectRatio: false,
+        options: {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero: true
+                    }
+                }]
+            }
         }
+        });
     }
-    });
-
     openCloseSidebar();
 
     function openCloseSidebar(){
@@ -90,4 +132,21 @@ $(document).ready(function(){
     });
 
 
+
+    /* add or remove product button */
+    $('.product-selector .button-selector').on('click', function(){        
+        var oldValue = $(this).siblings('input').val();
+
+        /* add */   
+        if ( $(this).hasClass("add") ){
+            var newValue = parseInt(oldValue) + 1;
+            $(this).siblings('input').val(newValue);
+        }
+        /* remove */
+        else if ( $(this).hasClass("remove") ){
+            // verify if number of products is not under 0
+            var newValue = ( (parseInt(oldValue) - 1) <= 0 ) ? 0 : ( parseInt(oldValue) - 1 );
+            $(this).siblings('input').val(newValue);
+        }
+    });
 })
